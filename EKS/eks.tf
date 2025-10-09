e658e3f7e3b6215f706c25db8d3bbaa060299835
ssgaik752 <127913640+ssgaik752@users.noreply.github.com>
2025-10-10 00:10:30 +0530

    Update eks.tf

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -1,6 +1,7 @@
 module "eks" {
-  source                         = "terraform-aws-modules/eks/aws"
-  version                        = "~> 21.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
   name                   = "eks-cluster"
   kubernetes_version                = "1.33"
   addons = {
@@ -15,18 +16,33 @@ module "eks" {
   }
   endpoint_public_access = true
   enable_cluster_creator_admin_permissions = true
-  vpc_id                         = module.my-vpc.vpc_id
-  subnet_ids                     = module.my-vpc.private_subnets
-  tags = {
-    environment = "development"
-    application = "myapp"
-  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  # control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # # EKS Managed Node Group(s)
+  # eks_managed_node_group_defaults = {
+  #   ami_type       = "AL2_x86_64"
+  #   instance_types = ["m5.large"]
+
+  #   attach_cluster_primary_security_group = true
+  # }
+
   eks_managed_node_groups = {
-    dev = {
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
-      instance_types = ["t3.micro"]
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "helloworld"
+      }
     }
   }
+
+  tags = local.tags
 }
