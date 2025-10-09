1e84342c09566c350ffa99ea9ed7719ce600b72d
ssgaik752 <127913640+ssgaik752@users.noreply.github.com>
2025-10-09 18:28:36 +0530

    Update eks.tf

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -1,48 +1,32 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
-
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
-
-  cluster_addons = {
-    coredns = {
-      most_recent = true
+  source                         = "terraform-aws-modules/eks/aws"
+  version                        = "~> 21.0"
+  name                   = "eks-cluster"
+  kubernetes_version                = "1.33"
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
     }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
     }
   }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  # control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # # EKS Managed Node Group(s)
-  # eks_managed_node_group_defaults = {
-  #   ami_type       = "AL2_x86_64"
-  #   instance_types = ["m5.large"]
-
-  #   attach_cluster_primary_security_group = true
-  # }
-
+  endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+  vpc_id                         = module.my-vpc.vpc_id
+  subnet_ids                     = module.my-vpc.private_subnets
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
   eks_managed_node_groups = {
-    amc-cluster-wg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
+    dev = {
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+      instance_types = ["t3.micro"]
     }
   }
-
-  tags = local.tags
 }
